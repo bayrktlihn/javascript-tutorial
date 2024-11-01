10ae5e4d1923072e9597e2ee2fbe3c173b6b6871
alihan.bayraktar <alihan.bayraktar@icisleri.gov.tr>
2024-11-01 12:02:24 +0300

    first commit

--- common.js
+++ common.js
@@ -29,7 +29,7 @@ const FormUtil = {
 
         for (let input of inputs) {
 
-            if (this.isButton(input)) {
+            if (FormInputUtil.isButton(input)) {
                 continue;
             }
 
@@ -58,21 +58,6 @@ const FormUtil = {
         }
 
         return data;
-    },
-    isButton(el) {
-        if (!el instanceof HTMLInputElement && !el instanceof HTMLButtonElement) {
-            return false;
-        }
-
-        if (el instanceof HTMLButtonElement) {
-            return true;
-        }
-
-        if (el instanceof HTMLInputElement && !!['submit', 'button', 'reset'].find(item => item === el.type)) {
-            return true;
-        }
-
-        return false;
     }
 
 
